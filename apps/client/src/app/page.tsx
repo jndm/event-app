7b739cbf58e4dac536e2507ae958def8410aa361
jndm <joonas.j.merilainen@gmail.com>
2024-06-11 22:08:03 +0300

    webhook test

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   return (
     <div>
       <h1>Landing page</h1>
-      <p>Welcome to events app .</p>
+      <p>Welcome to events app!</p>
     </div>
   );
 }
